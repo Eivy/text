c43b6adea27310f911a38a83c85ff91126aedbb9
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-01-31 10:27:18 +0900

    init

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		print(os.Stdin)
+	} else {
+		for _, p := range os.Args[1:] {
+			f, err := os.Open(p)
+			if err != nil {
+				panic(err)
+			}
+			print(f)
+		}
+	}
+}
+
+func print(f *os.File) {
+	re := regexp.MustCompile("\033\\[[0-9]{0,2}m")
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		fmt.Println(re.ReplaceAllString(s.Text(), ""))
+	}
+}
